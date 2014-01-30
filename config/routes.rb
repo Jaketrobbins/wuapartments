20de7168224296b0f825c656e016a705ce4fe1c8
Jake Robbins <jaketrobbins90@gmail.com>
2014-01-30 11:58:09 -0600

    about,help,home,and edit pages

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,7 @@
 #static pages
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
+  match '/contact', to: 'static_pages#contact', via: 'get'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
